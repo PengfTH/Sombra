027db60205cdc8ece1657b11f5fc09d5a8d63773
yz3060 <jzqh0110@gmail.com>
2017-04-01 15:47:57 -0400

    similarity computing

--- src/main/java/edu/cu/cs/Sombra/Schema/TemplateFeature.java
+++ src/main/java/edu/cu/cs/Sombra/Schema/TemplateFeature.java
@@ -1,13 +1,49 @@
 package edu.cu.cs.Sombra.Schema;
 
+import edu.cu.cs.Sombra.DomTree.DomTreeNode;
+import info.debatty.java.stringsimilarity.Levenshtein;
+
 public abstract class TemplateFeature {
 	public String tagPath;
 	public String vPath;
 	public double vWeight;
+	public String id;
 	
-	public TemplateFeature(String tagPath, String vPath, double vWeight) {
+	public final double tagPathPara = 1;
+	public final double vPathPara = 1;
+	public final double vWeightPara = 1;
+	public final double contentPara = 1;
+	public final double simThreshold = (double) 2;
+	
+	protected Levenshtein leven = new Levenshtein();
+
+	
+	public TemplateFeature(String tagPath, String vPath, double vWeight, String id) {
 		this.tagPath = tagPath;
 		this.vPath = vPath;
 		this.vWeight = vWeight;
+		this.id = id;
+	}
+	
+	public double similarity(DomTreeNode node) {
+		double res = 0;
+
+		if (!node.getId().isEmpty() && node.getId().equals(this.id)) {
+			return 1000;
+		}
+
+		// Tag Path
+		double editD = this.leven.distance(node.getTagPathString(), this.tagPath);
+		res += this.tagPathPara * Math.exp(-editD / 8.0);
+
+		// Visual Path
+		if (node.getVPath().equals(this.vPath)) {
+			res += this.vPathPara;
+		}
+
+		// Visual Weight
+		res += this.vWeightPara * Math.exp(-Math.abs(node.getVWeight() - this.vWeight));
+		
+		return res;
 	}
 }
--- src/main/java/edu/cu/cs/Sombra/Schema/ValueNode.java
+++ src/main/java/edu/cu/cs/Sombra/Schema/ValueNode.java
@@ -1,15 +1,23 @@
 package edu.cu.cs.Sombra.Schema;
 
+import edu.cu.cs.Sombra.DomTree.DomTreeNode;
+
 public class ValueNode extends TemplateFeature {
 	public String content;
 	
-	public ValueNode(String tagPath, String vPath, double vWeight, String content) {
-		super(tagPath, vPath, vWeight);
+	public ValueNode(String tagPath, String vPath, double vWeight, String id, String content) {
+		super(tagPath, vPath, vWeight, id);
 		this.content = content;
 	}
 	
-	public void getNameNode() {
+	public double similarity(DomTreeNode node) {
+		double res = 0;
+		res += super.similarity(node);
 		
+		//content
+		double editD = this.leven.distance(node.getContent(), this.content);
+		res += this.contentPara * Math.exp(-editD / 2.0);
+		return res;
 	}
 
 }
