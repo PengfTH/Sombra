a122d8b1a9dc6e9dda29fadaab64f50c396a6a3e
yz3060 <jzqh0110@gmail.com>
2017-04-15 17:35:09 -0400

    configure phantomjs

--- src/main/java/edu/cu/cs/Sombra/Config.java
+++ src/main/java/edu/cu/cs/Sombra/Config.java
@@ -1,6 +1,6 @@
 package edu.cu.cs.Sombra;
 
 public class Config {
-	public final static String PHANTOMJS_PATH = "/Users/feipeng/Downloads/bin/phantomjs";
+	public final static String PHANTOMJS_PATH = "./bin/phantomjs";
 	
 }
